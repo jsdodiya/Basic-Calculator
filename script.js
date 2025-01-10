4d8f92b148264edf523205d571a4f36326bb225d
jsdodiya <jinendradodiya9@gmail.com>
2025-01-10 14:24:07 +0530

    80% done

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Todo: Make M+ M- and MC functional 
+document.getElementById('lets-calculate-btn').addEventListener('click', function() {
+    document.getElementById('welcome-section').style.display = 'none';    
+    document.getElementById('calculator-section').style.display = 'block';
+});
+
 let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-           string = eval(string);
-           document.querySelector('input').value = string; 
-        }
-        else if(e.target.innerHTML == 'C'){
-           string = "";
-           document.querySelector('input').value = string; 
-        }
-        else{
-            console.log(e.target)
-            string=string+ e.target.innerHTML;
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        } else {
+            string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
         }
-        
     })
-})
+});
